e34098bf0f175e8e6ae403c4e7d296f7b8f01e0b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-23 12:16:37 +0100

    Update Email.java

--- spring-messaging-jms/src/main/java/com/shotaroi/springmessagingjms/Email.java
+++ spring-messaging-jms/src/main/java/com/shotaroi/springmessagingjms/Email.java
@@ -1,4 +1,38 @@
 package com.shotaroi.springmessagingjms;
 
 public class Email {
+    private String to;
+    private String body;
+
+    public Email() {
+    }
+
+    public Email(String to, String body) {
+        this.to = to;
+        this.body = body;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "to='" + to + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
