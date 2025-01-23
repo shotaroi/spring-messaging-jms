7bed26cf162ab2c1cf3c2fdb87ffc9ac5d372572
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-23 12:14:26 +0100

    Create project

---
+++ spring-messaging-jms/src/main/java/com/shotaroi/springmessagingjms/SpringMessagingJmsApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springmessagingjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMessagingJmsApplication.class, args);
+    }
+
+}
