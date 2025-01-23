04865c225ee61710c94f0553140707f4533128ce
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-23 12:22:22 +0100

    Update Receiver.java

--- spring-messaging-jms/src/main/java/com/shotaroi/springmessagingjms/Receiver.java
+++ spring-messaging-jms/src/main/java/com/shotaroi/springmessagingjms/Receiver.java
@@ -1,4 +1,12 @@
 package com.shotaroi.springmessagingjms;
 
+import org.springframework.stereotype.Component;
+import org.springframework.jms.annotation.JmsListener;
+
+@Component
 public class Receiver {
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
 }
